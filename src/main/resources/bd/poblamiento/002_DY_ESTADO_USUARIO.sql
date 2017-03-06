66e34ce6eec7b2156053df36b3220f88f9058ba6
dyanez <dyanez@neobits.iptv.microsoft.com>
2017-03-05 21:54:35 -0300

    - estado de usuario

---
+++ src/main/resources/bd/poblamiento/002_DY_ESTADO_USUARIO.sql
@@ -0,0 +1,7 @@
+/*==============================================================*/
+/*ESTADO DE USUARIO   */
+/*==============================================================*/
+
+INSERT INTO ESTADO_USUARIO(ID_ESTADO_USUARIO,NOMBRE) VALUES(1,'nuevo');
+INSERT INTO ESTADO_USUARIO(ID_ESTADO_USUARIO,NOMBRE) VALUES(2,'activo');
+INSERT INTO ESTADO_USUARIO(ID_ESTADO_USUARIO,NOMBRE) VALUES(3,'inactivo');
